eb3ee7a9a6dd4f00585466b106236e413316f781
nurettinBayram <fakhannurettin@gmail.com>
2025-09-19 20:34:12 -0400

    History dosyasi eklendi Kaldigimiz yerden devam edelim

---
+++ PycharmProjects/AI/History.py
@@ -0,0 +1,14 @@
+# name = "nurettin"
+# age = 25
+# print(f"My name is {name} and i am {age} years old.")
+
+
+# numbers = [2,4,6,8,10]
+# for i in numbers:
+#     print(i**2)
+
+
+i = input("Enter a word : ")
+x = i[::-1]
+
+
---
+++ PycharmProjects/LessonUdemy/OOP.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+timmy =Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(200)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(400)
+
+
+my_screen =Screen()
+my_screen.exitonclick()
+
